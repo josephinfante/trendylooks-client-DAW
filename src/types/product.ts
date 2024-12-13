7b79bb8ebc5b044101336ae16e8347d9d4253a42
Joseph Infante <josephinfante.y@gmail.com>
2024-12-12 20:54:02 -0500

    adding types

---
+++ src/types/product.ts
@@ -0,0 +1,11 @@
+export type Product = {
+  codProd: number;
+  nomProd: string;
+  descProd: string;
+  codCat: number;
+  preProd: number;
+  stockProd: number;
+  imgProd: string;
+  estProd: boolean;
+  fecProd: string;
+};

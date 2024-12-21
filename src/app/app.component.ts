d44216c43d1da2cb533429054bf04112777ba9af
Joseph Infante <josephinfante.y@gmail.com>
2024-12-21 01:16:13 -0500

    adding header and footer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { NavComponent } from './components/nav/nav.component'
+import { FooterComponent } from './components/footer/footer.component'
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  standalone: true,
+	selector: 'app-root',
+	imports: [RouterOutlet, NavComponent, FooterComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css',
+	standalone: true,
 })
 export class AppComponent {
-  title = 'trendylooks-client-DAW';
+	title = 'trendylooks-client-DAW'
 }

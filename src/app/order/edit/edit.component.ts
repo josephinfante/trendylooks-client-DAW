18453d3e5d470020a4cd33beac67bae993c41de5
Joseph Infante <josephinfante.y@gmail.com>
2024-12-21 17:50:32 -0500

    listing user into order create

--- src/app/order/edit/edit.component.ts
+++ src/app/order/edit/edit.component.ts
@@ -6,6 +6,10 @@ import { ToastrService } from 'ngx-toastr'
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 import { BackButtonDirective } from '../../directives/back-button.directive'
 import { CommonModule } from '@angular/common'
+import { formatDateForInput } from '../../../utils/format-date'
+import { UserService } from '../../services/user.service'
+import { FindAllResponse } from '../../../types/find-all-response'
+import { User } from '../../../types/user'
 
 @Component({
 	selector: 'order-edit',
@@ -18,6 +22,7 @@ export class OrderEditComponent implements OnInit {
 	isEdit: boolean = true
 	orderId: number | null = null
 	orderForm: FormGroup
+	users: FindAllResponse<User> | null = null
 
 	constructor(
 		private fb: FormBuilder,
@@ -25,16 +30,17 @@ export class OrderEditComponent implements OnInit {
 		private orderService: OrderService,
 		private router: Router,
 		private toastr: ToastrService,
+		private userService: UserService,
 	) {
 		this.orderForm = this.fb.group({
-			codPed: ['', Validators.required],
+			codPed: [''],
 			codUsu: ['', Validators.required],
 			totPed: ['', [Validators.required, Validators.min(1)]],
 			contPed: ['', Validators.required],
 			telPed: ['', [Validators.required]],
 			dirPed: ['', Validators.required],
-			estPed: ['Pendiente', Validators.required],
-			fecPed: [{ value: new Date(), disabled: true }],
+			estPed: ['Pendiente'],
+			fecPed: [{ value: new Date() }],
 		})
 	}
 
@@ -42,6 +48,7 @@ export class OrderEditComponent implements OnInit {
 		this.orderId = this.activatedRoute.snapshot.params['id']
 		this.isEdit = !!this.orderId
 		if (this.orderId) this.getData()
+		this.getUsers()
 	}
 
 	public getData(): void {
@@ -50,12 +57,19 @@ export class OrderEditComponent implements OnInit {
 			next: (res) =>
 				this.orderForm.patchValue({
 					...(res.body as Order),
-					fecPed: new Date((res.body as Order).fecPed),
+					fecPed: formatDateForInput((res.body as Order).fecPed),
 				}),
 			error: (err) => {
 				this.router.navigate(['/order'])
 				this.toastr.error(err.error.message)
 			},
+		})
+	}
+
+	public getUsers(): void {
+		this.userService.getAllUsers(1, 100).subscribe({
+			next: (res) => (this.users = res.body as FindAllResponse<User>),
+			error: (err) => this.toastr.error(err.error.message),
 		})
 	}
 
@@ -74,14 +88,16 @@ export class OrderEditComponent implements OnInit {
 
 	public update(): void {
 		if (!this.orderId) return
-		this.orderService.updateOrder(this.orderId, this.orderForm.value).subscribe({
-			next: (res) => {
-				const response = res.body as { message: string }
-				this.toastr.success(response.message)
-				this.router.navigate(['/order'])
-			},
-			error: (err) => this.toastr.error(err.error.message),
-		})
+		this.orderService
+			.updateOrder(this.orderId, { ...this.orderForm.value, fecPed: new Date(this.orderForm.value.fecPed) })
+			.subscribe({
+				next: (res) => {
+					const response = res.body as { message: string }
+					this.toastr.success(response.message)
+					this.router.navigate(['/order'])
+				},
+				error: (err) => this.toastr.error(err.error.message),
+			})
 	}
 
 	public onSubmit(): void {
